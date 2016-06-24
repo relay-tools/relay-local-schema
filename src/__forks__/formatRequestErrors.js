d064150d095551194b0078d9d5dcf9efa6b790af
Jimmy Jia <tesrin@gmail.com>
2016-06-24 18:58:34 -0400

    Update forked formatRequestErrors

--- src/__forks__/formatRequestErrors.js
+++ src/__forks__/formatRequestErrors.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 /**
- * Copyright 2013-2015, Facebook, Inc.
+ * Copyright (c) 2013-present, Facebook, Inc.
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
@@ -13,27 +13,27 @@
  * Formats an error response from GraphQL server request.
  */
 export default function formatRequestErrors(request, errors) {
-  var CONTEXT_BEFORE = 20;
-  var CONTEXT_LENGTH = 60;
+  const CONTEXT_BEFORE = 20;
+  const CONTEXT_LENGTH = 60;
 
-  var queryLines = request.getQueryString().split('\n');
+  const queryLines = request.getQueryString().split('\n');
   return errors.map(({locations, message}, ii) => {
-    var prefix = (ii + 1) + '. ';
-    var indent = ' '.repeat(prefix.length);
+    const prefix = (ii + 1) + '. ';
+    const indent = ' '.repeat(prefix.length);
 
     //custom errors thrown in graphql-server may not have locations
-    var locationMessage = locations ?
+    const locationMessage = locations ?
       ('\n' + locations.map(({column, line}) => {
-        var queryLine = queryLines[line - 1];
-        var offset = Math.min(column - 1, CONTEXT_BEFORE);
+        const queryLine = queryLines[line - 1];
+        const offset = Math.min(column - 1, CONTEXT_BEFORE);
         return [
           queryLine.substr(column - 1 - offset, CONTEXT_LENGTH),
-          ' '.repeat(offset) + '^^^'
+          ' '.repeat(Math.max(0, offset)) + '^^^',
         ].map(messageLine => indent + messageLine).join('\n');
-        }).join('\n')) :
+      }).join('\n')) :
       '';
 
-      return prefix + message + locationMessage;
+    return prefix + message + locationMessage;
 
   }).join('\n');
 }
