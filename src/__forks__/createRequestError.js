f92bac0e16689dc273bb8bc899be2adb4d77c6ac
Rodrigo Moyle <rodrigorm@gmail.com>
2016-11-21 00:20:47 -0200

    Added FB copyright

--- src/__forks__/createRequestError.js
+++ src/__forks__/createRequestError.js
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+/**
+ * Formats an error response from GraphQL server request.
+ */
 import formatRequestErrors from './formatRequestErrors';
 
 export default function createRequestError(request, requestType, payload) {
